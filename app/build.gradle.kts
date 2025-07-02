eddb20c55d3192781b071b67bb83dac2f6823ed4
ewfawfasdf <103755103+ewfawfasdf@users.noreply.github.com>
2025-07-02 16:36:35 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,14 +35,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    // Отключаем Compose, так как мы используем XML
     buildFeatures {
         compose = false
     }
 }
 
 dependencies {
-    // Используем правильный синтаксис с круглыми скобками
+    
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
@@ -50,8 +49,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics.android)
     implementation(libs.androidx.foundation.android)
     implementation(libs.androidx.material3.android)
-
-    // Стандартные библиотеки для тестирования
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
